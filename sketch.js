ab1495473d8627b74c2dbd573dc8f289e7281ebd
Keerthi3275 <70685704+Keerthi3275@users.noreply.github.com>
2021-12-01 13:29:21 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+//Create variable for ground,lander,landerimage & bgimage
+
+
+//Initialize variables for vx,vy & g
+
+
+function preload()
+{
+  //Load landerimg & bgimg
+ 
+}
+
+function setup() 
+{
+  //Write instruction to create canvas
+
+
+  //create sprite for the lander
+  
+  //Add image for the lander
+  
+
+  
+}
+
+function draw() 
+{
+  background(51);
+  //add background image
+  
+
+  //write text instruction to display vertical velocity
+  
+
+  //write code to add gravity to the lander
+
+ 
+
+  //write code to display the sprites
+ 
+}
+
+
